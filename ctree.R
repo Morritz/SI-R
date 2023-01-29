d09d597d77fc1a6ba648b331f411c012b1a58248
Morritz <12800230+Morritz@users.noreply.github.com>
2023-01-30 00:33:52 +0100

    ctree

---
+++ ctree.R
@@ -0,0 +1,37 @@
+#CTree
+library(caret)
+library(class)
+library(party)
+
+# Wczytanie zbioru danych
+data <- read.csv("https://raw.githubusercontent.com/Morritz/SI-R/main/heart.csv", header = TRUE)
+labels <- colnames(data)
+
+output_label <- labels[ncol(data)]
+input_labels <- labels[-ncol(data)]
+
+n <- 100 # Iteracje
+acc_list <- list()
+proporcja <- 0.2
+
+for(i in 1:n){
+  
+  # Podział zbioru danych na zbiór treningowy i testowy
+  split <- createDataPartition(data$output, p = proporcja, list = FALSE)
+  train_data <- data[split, ]
+  test_data <- data[-split, ]
+  
+  # Dopasowanie modelu CTree do danych treningowych
+  model_ctree <- ctree(output ~ ., train_data)
+  predictions <- predict(model_ctree, test_data)
+  predictions_binary <- ifelse(predictions > 0.5, 1, 0)
+  confusion_matrix <- table(test_data$output, predictions_binary)
+  print(confusion_matrix)
+  accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+  acc_list <- append(acc_list, accuracy)
+  print(paste("Accuracy #", i, accuracy))
+}
+print(paste("SD of accuracy", sd(as.double(acc_list))))
+print(paste("Mean of accuracy", mean(as.double(acc_list))))
+
+
